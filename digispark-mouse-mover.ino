a487d5e1c2667de22c866b454116d67256364472
strupo <matthew.b.t.1@gmail.com>
2017-04-18 15:32:14 -0400

    Update digispark-mouse-mover.ino

--- digispark-mouse-mover.ino
+++ digispark-mouse-mover.ino
@@ -7,9 +7,9 @@ void setup() {
 
 void loop() {
     DigiMouse.move(1,0,0,0); //Start mouse moving
-        DigiMouse.delay(250000);
+        DigiMouse.delay(25);
         DigiMouse.move(-1,0,0,0); //Move it back
-        DigiMouse.delay(250000);
+        DigiMouse.delay(300000); //wait 5 minutes
         DigiMouse.move(0,0,0,0); //Stop moving?
 
 }
